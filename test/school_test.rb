e314a8b3bea9d30d797124c60357cf21c3401c7a
Zach Holcomb <zachholcombmusic@gmail.com>
2020-01-29 13:49:21 -0700

    Add new test to school_test.rb

--- test/school_test.rb
+++ test/school_test.rb
@@ -11,21 +11,20 @@ class SchoolTest < Minitest::Test
   end
 
   def test_it_has_start_time
-    skip
     school = School.new('9:00', 7)
 
     assert_equal '9:00', school.start_time
   end
 
   def test_it_has_hours_in_school_day
-    skip
+
     school = School.new('9:00', 7)
 
     assert_equal 7, school.hours_in_school_day
   end
 
   def test_it_starts_with_no_student_names
-    skip
+
     school = School.new('9:00', 7)
 
     assert_equal [], school.student_names
@@ -33,7 +32,7 @@ class SchoolTest < Minitest::Test
 
   #Iteration 2 Tests:
   def test_it_can_add_student_names
-    skip
+
     school = School.new('9:00', 7)
 
     school.add_student_name('Aurora')
@@ -44,11 +43,28 @@ class SchoolTest < Minitest::Test
   end
 
   def test_it_can_calculate_end_time
-    skip
     school1 = School.new('9:00', 7)
     school2 = School.new('9:00', 3)
 
     assert_equal '16:00', school1.end_time
     assert_equal '12:00', school2.end_time
+  end
+  def test_school_is_full_time
+    school1 = School.new('9:00', 7)
+    school2 = School.new('9:00', 3)
+
+    assert_equal true, school1.is_full_time?
+    assert_equal false, school2.is_full_time?
+  end
+  def test_school_standard_student_names
+    school1 = School.new('9:00', 7)
+
+    school1.add_student_name('Aurora')
+    school1.add_student_name('tim')
+    school1.add_student_name('megan')
+
+    names_test = ["Aurora", "Tim", "Megan"]
+
+    assert_equal names_test, school1.standard_student_names
   end
 end
