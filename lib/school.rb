a544df64e52102ff69bc6441be40da1eb8769277
Zach Holcomb <zachholcombmusic@gmail.com>
2020-01-29 13:50:48 -0700

    Add standard_student_names method to school.rb

---
+++ lib/school.rb
@@ -0,0 +1,36 @@
+class School
+  attr_reader :start_time, :hours_in_school_day, :student_names
+
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+  end
+
+  def add_student_name(student)
+    @student_names << student
+  end
+
+  def end_time
+    time1 = @start_time.to_i
+    time_end = time1 + @hours_in_school_day
+    time_final = "#{time_end}:00"
+    return time_final
+  end
+
+  def is_full_time?
+    if @hours_in_school_day > 4
+      true
+    else
+      false
+    end
+  end
+
+  def standard_student_names
+    @student_names.collect { |name| name.capitalize }
+  end
+
+  
+
+
+end
